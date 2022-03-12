7f29b852a62358890ed40e9877978bfde57da098
wyao <yaowang@melco-resorts.com>
2022-03-12 22:08:59 +0800

    code base

---
+++ src/main/java/com/thoughtworks/airticketorder/AirTicketOrderApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.airticketorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTicketOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirTicketOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thoughtworks/airticketorder/AirTicketOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.airticketorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTicketOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
