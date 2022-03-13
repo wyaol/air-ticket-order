1daac604b165b9432e563b27e7e3554aa68d6be1
wyao <yaowang@melco-resorts.com>
2022-03-13 15:58:24 +0800

    story1-ac1-ex2-step3: 工序4，stub Repository和stub Client， 当调用Repository的save时 成功返回，当调用lockInventory抛出ThirdServiceException，调用getFlightRequest时成功返回；实现Service保障一致性首次请求失败发送get请求成功获取到订单号。

--- src/main/java/com/thoughtworks/airticketorder/service/OrderService.java
+++ src/main/java/com/thoughtworks/airticketorder/service/OrderService.java
@@ -3,7 +3,9 @@ package com.thoughtworks.airticketorder.service;
 import com.thoughtworks.airticketorder.client.InventoryTicketPriceClient;
 import com.thoughtworks.airticketorder.client.request.InventoryLockRequest;
 import com.thoughtworks.airticketorder.client.response.ClientResponse;
+import com.thoughtworks.airticketorder.client.response.FlightRequestResponse;
 import com.thoughtworks.airticketorder.client.response.InventoryLockResponse;
+import com.thoughtworks.airticketorder.exceptions.ThirdServiceException;
 import com.thoughtworks.airticketorder.repository.OrderRepository;
 import com.thoughtworks.airticketorder.repository.entity.OrderEntity;
 import com.thoughtworks.airticketorder.service.dto.OrderCreate;
@@ -21,14 +23,23 @@ public class OrderService {
     private OrderRepository orderRepository;
 
     public OrderCreated createOrder(OrderCreate orderCreate) {
-        final ClientResponse<InventoryLockResponse> response = inventoryTicketPriceClient.lockInventory(new InventoryLockRequest(
-                orderCreate.getFlightId(), orderCreate.getClassType(), UUID.randomUUID().toString()
-        ));
+        final String requestId = UUID.randomUUID().toString();
+        String flightOrderId;
+        try {
+            final ClientResponse<InventoryLockResponse> response = inventoryTicketPriceClient.lockInventory(new InventoryLockRequest(
+                    orderCreate.getFlightId(), orderCreate.getClassType(), requestId
+            ));
+            flightOrderId = response.getData().getFlightOrderId();
+        } catch (ThirdServiceException e) {
+            final ClientResponse<FlightRequestResponse> response = inventoryTicketPriceClient.getFlightRequest(requestId);
+            flightOrderId = response.getData().getFlightOrderId();
+        }
+
         final OrderEntity orderEntity = orderRepository.save(OrderEntity.builder()
                 .insuranceOrderId(null)
                 .userId(orderCreate.getUserId())
                 .luggageWeightOrderId(null)
-                .flightOrderId(response.getData().getFlightOrderId())
+                .flightOrderId(flightOrderId)
                 .amount(orderCreate.getAmount())
                 .build());
         return OrderCreated.builder().id(orderEntity.getId()).build();
--- src/test/java/com/thoughtworks/airticketorder/service/OrderServiceTest.java
+++ src/test/java/com/thoughtworks/airticketorder/service/OrderServiceTest.java
@@ -2,8 +2,10 @@ package com.thoughtworks.airticketorder.service;
 
 import com.thoughtworks.airticketorder.client.InventoryTicketPriceClient;
 import com.thoughtworks.airticketorder.client.response.ClientResponse;
+import com.thoughtworks.airticketorder.client.response.FlightRequestResponse;
 import com.thoughtworks.airticketorder.client.response.InventoryLockResponse;
 import com.thoughtworks.airticketorder.dto.ClassType;
+import com.thoughtworks.airticketorder.exceptions.ThirdServiceException;
 import com.thoughtworks.airticketorder.repository.OrderRepository;
 import com.thoughtworks.airticketorder.repository.entity.OrderEntity;
 import com.thoughtworks.airticketorder.service.dto.OrderCreate;
@@ -45,6 +47,21 @@ class OrderServiceTest {
         when(inventoryTicketPriceClient.lockInventory(any())).thenReturn(
                 new ClientResponse<>(0, "", new InventoryLockResponse("5d8y6v"))
         );
+
+        final OrderCreated orderCreated = orderService.createOrder(
+                new OrderCreate(123, "flightId", ClassType.ECONOMY, BigDecimal.valueOf(2000)));
+
+        assertEquals(234, orderCreated.getId());
+    }
+
+    @Test
+    void shouldCreateOrderSuccessWhenGetLockInventoryResponseFailed() {
+        when(orderRepository.save(any())).thenReturn(
+                OrderEntity.builder().id(234) .build()
+        );
+        when(inventoryTicketPriceClient.lockInventory(any())).thenThrow(new ThirdServiceException());
+        when(inventoryTicketPriceClient.getFlightRequest(any())).thenReturn(
+                new ClientResponse<>(0, "", new FlightRequestResponse("5d8y6v")));
 
         final OrderCreated orderCreated = orderService.createOrder(
                 new OrderCreate(123, "flightId", ClassType.ECONOMY, BigDecimal.valueOf(2000)));
