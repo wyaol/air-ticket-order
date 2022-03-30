7b148e08d9e4dbe19a0785760d9fe9ca2ff0c436
wyao <yaowang@melco-resorts.com>
2022-03-31 00:15:50 +0800

    story2-ac1-ex1-step5: 工序5，fake Database，实现Repository调用数据库根据status findAll。

--- src/main/java/com/thoughtworks/airticketorder/repository/InvoiceRepository.java
+++ src/main/java/com/thoughtworks/airticketorder/repository/InvoiceRepository.java
@@ -1,9 +1,13 @@
 package com.thoughtworks.airticketorder.repository;
 
+import com.thoughtworks.airticketorder.dto.InvoiceStatus;
 import com.thoughtworks.airticketorder.repository.entity.InvoiceEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InvoiceRepository extends CrudRepository<InvoiceEntity, Integer> {
+    List<InvoiceEntity> findAllByStatus(InvoiceStatus status);
 }
--- src/test/java/com/thoughtworks/airticketorder/repository/InvoiceRepositoryTest.java
+++ src/test/java/com/thoughtworks/airticketorder/repository/InvoiceRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.thoughtworks.airticketorder.repository;
 
+import com.thoughtworks.airticketorder.dto.InvoiceStatus;
 import com.thoughtworks.airticketorder.repository.entity.InvoiceEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,5 +32,23 @@ public class InvoiceRepositoryTest {
                 invoiceEntity
         );
         assertEquals(invoiceEntity, invoiceEntity1);
+    }
+
+    @Test
+    void  shouldFindAllByStatus() {
+        InvoiceEntity invoiceEntity = InvoiceEntity.builder()
+                .address("address")
+                .bank("bank")
+                .bankAccount("bankAccount")
+                .email("email")
+                .number("number")
+                .flightOrderId("flightOrderId")
+                .status(InvoiceStatus.ISSUED)
+                .build();
+        invoiceRepository.save(invoiceEntity);
+
+        final List<InvoiceEntity> invoiceEntities = invoiceRepository.findAllByStatus(InvoiceStatus.ISSUED);
+
+        assertEquals(1, invoiceEntities.size());
     }
 }
